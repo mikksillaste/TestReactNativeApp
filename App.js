4d3838d8fad36c4cb1788b92fc29e9e4769795ff
mikksillaste <mikk.sillaste@gmail.com>
2018-01-15 08:18:01 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+// Sets up a platform-specific display message.
+const instructions = Platform.select({
+
+});
+
+// Defines the component that represents the UI.
+export default class App extends Component<{}> {
+
+}
+
+// Creates a style object that controls the component’s layout and appearance.
+const styles = StyleSheet.create({
+
+});
